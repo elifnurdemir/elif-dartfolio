d46ef303dd53877e632a1facd727aaa23ba281de
Mert Metin <necatimertmetin@gmail.com>
2025-02-18 10:41:48 +0300

    fix: browserRouter basename updated

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/elif-dartfolio">
     <App />
   </BrowserRouter>
 );
