a22ed3f339eeaa0821fccca94ea058b6b8faf859
Mert Metin <necatimertmetin@gmail.com>
2025-03-08 18:39:21 +0300

    suna ne dersin

--- src/components/interface/Phone.tsx
+++ src/components/interface/Phone.tsx
@@ -1,7 +1,7 @@
 import { Stack, Avatar, Box } from "@mui/material";
 import { motion } from "framer-motion";
 import { useTheme } from "@mui/material/styles";
-import PhoneImg from "../../assets/Phone.png";
+import PhoneImg from "../../assets/YeniTelefon.png";
 import flutter from "../../assets/flutter.svg";
 import kotlin from "../../assets/kotlin.svg";
 import swift from "../../assets/swift.svg";
@@ -23,20 +23,41 @@ export const Phone = ({ darkMode, setDarkMode }: LigthSwitchProps) => {
         transition={{ duration: 0.5, ease: [0.25, 0.46, 0.45, 0.94] }}
         sx={{
           position: "absolute",
-          top: "20%",
-          left: 0,
-          backgroundImage: `url(${PhoneImg}) !important`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "contain",
+          top: "22%",
           height: "775px",
-          width: "389px",
+          width: "350px",
           overflow: "hidden",
-          filter: "drop-shadow(-50px 0px 30px rgba(0, 0, 0, 0.5))",
+          "&::before": {
+            content: '""',
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            margin: 1,
+            backdropFilter: "blur(10px)", // Blur efekti
+            zIndex: 0, // Arkada kalmasını sağlıyoruz
+            pointerEvents: "none",
+            borderRadius: "100px !important",
+          },
+          "&::after": {
+            content: '""',
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundImage: `url(${PhoneImg})`, // Görüntüyü After’a ekledik
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+            zIndex: 1, // Asıl içerikten önce ama blur'dan sonra olmalı
+            pointerEvents: "none",
+          },
         }}
         direction={"column"}
         alignItems={"center"}
         pt={20}
-        gap={8}
+        spacing={8}
       >
         <Avatar
           alt="Elifnur Demir"
@@ -60,7 +81,7 @@ export const Phone = ({ darkMode, setDarkMode }: LigthSwitchProps) => {
             transition={{ duration: 0.6, delay: 0.2 }}
             height={"64px"}
             src={flutter}
-            style={{ cursor: "pointer" }}
+            style={{ cursor: "pointer", zIndex: 1 }}
           />
           <motion.img
             initial={{ opacity: 0, y: 30 }}
@@ -68,7 +89,7 @@ export const Phone = ({ darkMode, setDarkMode }: LigthSwitchProps) => {
             transition={{ duration: 0.6, delay: 0.4 }}
             height={"64px"}
             src={swift}
-            style={{ cursor: "pointer" }}
+            style={{ cursor: "pointer", zIndex: 1 }}
           />
           <motion.img
             initial={{ opacity: 0, y: 30 }}
@@ -76,7 +97,7 @@ export const Phone = ({ darkMode, setDarkMode }: LigthSwitchProps) => {
             transition={{ duration: 0.6, delay: 0.6 }}
             height={"64px"}
             src={kotlin}
-            style={{ cursor: "pointer" }}
+            style={{ cursor: "pointer", zIndex: 1 }}
           />
         </Stack>
       </Stack>
