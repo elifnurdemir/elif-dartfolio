829aa3323305873cc18cbd03b651fb65a8797bb1
Mert Metin <necatimertmetin@gmail.com>
2025-03-08 17:36:39 +0300

    switch updated

--- src/components/interface/LightSwitch.tsx
+++ src/components/interface/LightSwitch.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { useTheme } from "@mui/material/styles";
+import { DarkMode, LightMode } from "@mui/icons-material";
 
 const StyledWrapper = styled.div`
   .theme-switch {
@@ -44,19 +45,14 @@ const StyledWrapper = styled.div`
     z-index: 10;
     transition: 0.5s cubic-bezier(1, 0.33, 0.11, 1.34);
     background-color: ${(props) => props.theme.palette.text.primary};
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   #theme-checkbox:checked + label div {
     left: 1.6em;
     background-color: ${(props) => props.theme.palette.text.secondary};
-  }
-
-  #theme-checkbox + label span:first-of-type {
-    color: ${(props) => props.theme.palette.text.primary};
-  }
-
-  #theme-checkbox + label span:last-of-type {
-    color: ${(props) => props.theme.palette.text.secondary};
   }
 `;
 
@@ -77,31 +73,13 @@ export const LightSwitch = ({ darkMode, setDarkMode }: LigthSwitchProps) => {
           onChange={() => setDarkMode(!darkMode)}
         />
         <label htmlFor="theme-checkbox">
-          <div />
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </span>
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
-            </svg>
-          </span>
+          <div>
+            {darkMode ? (
+              <DarkMode fontSize="small" sx={{ color: "#fff" }} />
+            ) : (
+              <LightMode fontSize="small" sx={{ color: "#fff" }} />
+            )}
+          </div>
         </label>
       </div>
     </StyledWrapper>
